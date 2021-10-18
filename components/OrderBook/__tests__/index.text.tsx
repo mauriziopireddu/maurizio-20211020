4816c64a0c6ceb980a7134da29818b10448b8603
Maurizio Pireddu <pireddu.maurizio@gmail.com>
2021-10-18 13:12:16 +0100

    test: remove act warning

--- components/OrderBook/__tests__/index.text.tsx
+++ components/OrderBook/__tests__/index.text.tsx
@@ -4,7 +4,9 @@ import { OrderBook } from "../OrderBook";
 describe("Home", () => {
   it("renders without crashing", async () => {
     render(<OrderBook />);
-    expect(screen.getByRole("heading", { name: /order book/i })).toBeVisible();
+    expect(
+      await screen.findByRole("heading", { name: /order book/i })
+    ).toBeVisible();
     const [bidsTable, asksTable] = screen.getAllByRole("table");
     expect(bidsTable).toBeVisible();
     expect(asksTable).toBeVisible();
